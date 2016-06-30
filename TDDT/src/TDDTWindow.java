48615394cc9c1fdc3a17c07b01b2ab6fc6303933
[Hakan] <[hague102@hhu.de]>
2016-06-30 20:13:19 +0200

    Neue Gui mit javafx - Hakan Gülmez

--- TDDT/src/TDDTWindow.java
+++ TDDT/src/TDDTWindow.java
@@ -1,98 +1,74 @@
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.JEditorPane;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.Color;
 
-public class TDDTWindow extends JFrame {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
-	private JPanel contentPane;
+public class TDDTWindow extends Application {
 
-	/**
-	 * Launch the application.
-	 */
+	@Override
+	public void start(Stage primaryStage) {
+		Button katalog = new Button("Wähle Katalog ");
+		Button aufgabe = new Button("Wähle Aufgabe");
+		Button phase   = new Button("Kompilieren");
+		Button nPhase  = new Button("Nächste Phase");
+		Button lPhase  = new Button("Letzte Phase    ");
+		
+		phase.setMinSize(50, 50);
+		phase.setStyle("-fx-border-color: black; -fx-background-color: red;");
+		
+		GridPane left = new GridPane(); 
+		left.setPadding(new Insets(10,10,10,10));
+		left.setVgap(5);
+		left.setHgap(5);
+		
+		left.add(katalog, 0, 0);
+		left.add(aufgabe, 0, 1);
+		
+		GridPane right = new GridPane(); 
+		right.setPadding(new Insets(10,10,10,10));
+		right.setVgap(5);
+		right.setHgap(5);
+		
+		right.add(nPhase, 0, 0);
+		right.add(lPhase, 0, 1);
+		
+		GridPane center = new GridPane();
+		center.setPadding(new Insets(10,10,10,10));
+		center.setVgap(5);
+		center.setHgap(5);
+		center.setAlignment(Pos.CENTER);
+		
+		center.add(phase, 0, 0);
+		
+		BorderPane top = new BorderPane(); 
+		top.setLeft(left);
+		top.setRight(right);
+		top.setCenter(center);
+		
+		TextArea editor = new TextArea();
+		
+		
+		BorderPane borderpane = new BorderPane();
+		borderpane.setTop(top);
+		borderpane.setCenter(editor);
+		
+		Scene scene = new Scene(borderpane, 700, 500);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("TDDT");
+		primaryStage.show();
+	}
+
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TDDTWindow frame = new TDDTWindow();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		launch(args);
 	}
-
-	/**
-	 * Create the frame.
-	 */
-	public TDDTWindow() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 700, 500);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		
-		JButton btnWhleAufgabe = new JButton("W\u00E4hle Aufgabe");
-		
-		JButton btnWhleKatalog = new JButton("W\u00E4hle Katalog");
-		
-		JButton btnNchstePhase = new JButton("N\u00E4chste Phase");
-		
-		JButton btnLetztePhase = new JButton("Letzte Phase");
-		
-		JButton btnKompiliere = new JButton("Kompiliere");
-		btnKompiliere.setForeground(Color.BLACK);
-		btnKompiliere.setBackground(Color.RED);
-		btnKompiliere.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		
-		JEditorPane editorPane = new JEditorPane();
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnWhleKatalog, GroupLayout.DEFAULT_SIZE, 123, Short.MAX_VALUE)
-						.addComponent(btnWhleAufgabe, GroupLayout.DEFAULT_SIZE, 123, Short.MAX_VALUE))
-					.addGap(174)
-					.addComponent(btnKompiliere)
-					.addGap(167)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-						.addComponent(btnLetztePhase, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(btnNchstePhase, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-				.addComponent(editorPane, GroupLayout.DEFAULT_SIZE, 672, Short.MAX_VALUE)
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(btnWhleKatalog)
-								.addComponent(btnLetztePhase))
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(btnWhleAufgabe)
-								.addComponent(btnNchstePhase)))
-						.addComponent(btnKompiliere, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-					.addGap(18)
-					.addComponent(editorPane, GroupLayout.DEFAULT_SIZE, 368, Short.MAX_VALUE))
-		);
-		contentPane.setLayout(gl_contentPane);
-	}
-
 }
