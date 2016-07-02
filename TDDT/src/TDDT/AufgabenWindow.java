b5df140385fc0b580c0adfe94d94e68c18d444bf
Gamer20896 <robin.ahlers@online.de>
2016-07-02 12:40:52 +0200

    Aufgaben einlesen funktioniert

---
+++ TDDT/src/TDDT/AufgabenWindow.java
@@ -0,0 +1,79 @@
+package TDDT;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class AufgabenWindow extends Application
+{
+	private String name;
+	private String testName;
+
+	@Override
+	public void start(Stage primaryStage)
+	{
+		Button aufgabe1 = new Button();
+		Button aufgabe2 = new Button();
+		Button aufgabe3 = new Button();
+		TaskReader tr = Window.getReader();
+		Aufgabe temp = tr.getAufgabe(0);
+		aufgabe1.setText(temp.getName());
+		temp = tr.getAufgabe(1);
+		aufgabe2.setText(temp.getName());
+		temp = tr.getAufgabe(2);
+		aufgabe3.setText(temp.getName());
+		
+		aufgabe1.setOnAction( event -> {
+			Aufgabe laden = tr.getAufgabe(0);
+			name = laden.getName();
+			testName = name + "Test.java";
+			name += ".java";
+			namenSetzen();
+			SaveLoad sl = new SaveLoad();
+			Window.aufgabenEinlesen(sl);
+			primaryStage.close();
+		});
+		
+		aufgabe2.setOnAction( event -> {
+			Aufgabe laden = tr.getAufgabe(1);
+			name = laden.getName();
+			testName = name + "Test.java";
+			name += ".java";
+			namenSetzen();
+			SaveLoad sl = new SaveLoad();
+			Window.aufgabenEinlesen(sl);
+			primaryStage.close();
+		});
+		
+		aufgabe3.setOnAction( event -> {
+			Aufgabe laden = tr.getAufgabe(2);
+			name = laden.getName();
+			testName = name + "Test.java";
+			name += ".java";
+			namenSetzen();
+			SaveLoad sl = new SaveLoad();
+			Window.aufgabenEinlesen(sl);
+			primaryStage.close();
+		});
+		
+		GridPane gp = new GridPane();
+		gp.setAlignment(Pos.CENTER);
+		gp.add(aufgabe1, 0, 0);
+		gp.add(aufgabe2, 1, 0);
+		gp.add(aufgabe3, 2, 0);
+		
+		Scene scene = new Scene(gp, 350, 200);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Aufgabe auswählen");
+		primaryStage.show();
+	}
+	
+	public void namenSetzen()
+	{
+		Window.setClassName(name);
+		Window.setClassTestName(testName);
+	}
+}
